501c31ad9795c3a45be03ea0321775392679d691
Egor <kolevegor@gmail.com>
2021-08-31 11:52:25 +0300

    year commit

--- leapyear.py
+++ leapyear.py
@@ -1,9 +1,12 @@
 # Leap Year
 
 year = int (input("Введите год :"))
-if year %4 == 0:
-    print("Высокосный")
-elif year %400 == 0:
-    print("Высокосный")
+if year % 4 != 0:
+    print("Не Високосный")
+elif year % 100 == 0:
+    if year % 400 == 0:
+        print("Високосный")
+    else:
+        print("Не Високосный")
 else:
-    print("Не Высокосный")
+    print("Високосный")
