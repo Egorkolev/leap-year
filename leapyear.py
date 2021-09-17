f6e2bcc144287cfc77c24442fb11bd5a058d2c95
MacBook-Egor <kolevegor@gmail.com>
2021-09-17 14:35:40 +0300

    add new

--- leapyear.py
+++ leapyear.py
@@ -1,12 +1,17 @@
 # Leap Year
-
-year = int (input("Введите год :"))
-if year % 4 != 0:
-    print("Не Високосный")
-elif year % 100 == 0:
-    if year % 400 == 0:
-        print("Високосный")
+l = int(input('Введите год:'))
+def leap(l):
+    if l % 100 == 0:
+        return True
+    if l % 400 == 0:
+        return True
+    if l % 4 != 0:
+        return False
     else:
-        print("Не Високосный")
-else:
-    print("Високосный")
+        return True
+print(leap(l))
+
+
+
+
+
